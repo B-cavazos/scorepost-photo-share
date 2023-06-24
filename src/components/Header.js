0b260593688618104f379585b8d13b56b9b0f1d4
Brittney Cavazos <brittneycavazos@Brittneys-MBP.lan>
2023-06-24 13:46:44 -0700

    feat(Header): make sticky

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ import{
 import {HomeIcon} from "@heroicons/react/24/solid"
 function Header() {
   return (
-    <div>
+    <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
         <div className='flex items-center justify-between max-w-6xl mx-5 lg:mx-auto'>
         {/* Left - icons */}
         <div className='relative hidden lg:inline-grid w-24 h-24 cursor-pointer'>
